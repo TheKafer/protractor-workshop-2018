ed713cc9a72e868f7de7a0ab239671760a1c2f23
TheKafer <38733280+TheKafer@users.noreply.github.com>
2020-12-16 09:17:29 -0500

    workshop finalizado

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,10 +3,15 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   },
   capabilities: {
--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,8 +1,18 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+const { AwesomeReport } = require('jasmine-awesome-report');
+
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY
     }
   }));
+
+  const config = {
+    fullPath: 'reports',
+    fileName: 'report',
+    merge: true
+  };
+
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
